7c10d733d02422ba035f7e5f7e5162b475bdbb10
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2024-05-23 15:56:27 -0300

    aula 2

---
+++ aula2.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = 'Thiago';
+  // print(name);
+  String lastName = 'Rocha';
+  // print(lastName);
+  // print('full name: ' + name + ' ' + lastName);
+  print("Nome completo: $name $lastName");
+}
